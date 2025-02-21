efbacb6a2391ff6103145e87b3165696a2227c7e
zlatan <1198492923@qq.com>
2025-02-21 21:20:10 +0800

    编辑员工代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -21,4 +21,13 @@ public interface EmployeeService {
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
