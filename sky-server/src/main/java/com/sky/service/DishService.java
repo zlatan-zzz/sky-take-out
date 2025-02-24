8548669821e8bc062246ca365ac1d1b07e61ab95
zlatan <1198492923@qq.com>
2025-02-24 22:23:55 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 
@@ -10,4 +12,6 @@ public interface DishService {
      * @param dishDTO
      */
     public void savewithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
